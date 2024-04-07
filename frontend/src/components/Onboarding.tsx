1665e2e125770bb26d700d334d58d6426b9903f9
finnwintz5 <102768648+finnwintz5@users.noreply.github.com>
2024-04-06 19:09:32 -0500

    stuff

--- frontend/src/components/Onboarding.tsx
+++ frontend/src/components/Onboarding.tsx
@@ -2,15 +2,16 @@ import { Chip } from "@mantine/core";
 import { useState } from "react";
 
 export function Onboarding({}) {
-    const [chipAns,setChipAns] = useState("react")
+  const [chipAns, setChipAns] = useState("react");
   return (
     <div>
       <Chip.Group multiple={false} value={chipAns} onChange={setChipAns}>
-      <Chip value="react">React</Chip>
-      <Chip value="ng">Angular</Chip>
-      <Chip value="svelte">Svelte</Chip>
-      <Chip value="vue">Vue</Chip>
-    </Chip.Group>
+        <Chip value="rideshare">Using rideshare</Chip>
+        <Chip value="walking">Walking alone</Chip>
+        <Chip value="emergency">Family Medical Emergency</Chip>
+        <Chip value="breakup">Friend breakup</Chip>
+        <Chip value="other">Other</Chip>
+      </Chip.Group>
     </div>
   );
 }
